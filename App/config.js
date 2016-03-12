dbcadc5e07e3defe781f0fc65ec9ca3348733d83
Adrian Preuß <info@adi-code.de>
2016-03-12 09:30:13 +0100

    change configuration

--- App/config.js
+++ App/config.js
@@ -2,6 +2,12 @@ var Config = {
 	Classes:	[ 'Popup' ],
 	Core:		[ 'Bank', 'UserInterface', 'ChannelTopic' ],
 	Features:	[],
-	Games:		[ 'Skyscraper', 'GodLadder' ],
-	Team:		[ 'Remuneration' ]
+	Games:		[ 'Skyscraper', 'GodLadder', 'SmileyCode' ],
+	Team:		[ 'Remuneration' ],
+	Settings:	{
+		Skyscraper: {
+			dice_maximum:	25,
+			knuddel:		[ 1, 2, 5, 10, 20 ]
+		}
+	}
 };
